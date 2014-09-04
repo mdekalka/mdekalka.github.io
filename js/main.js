95896f26d6f224700af6066a58d4d3f867f06cb5
Aleh_Isakau <Aleh_Isakau@epam.com>
2014-09-04 13:32:15 +0300

    add aside menu/ add footer

--- js/main.js
+++ js/main.js
@@ -8,6 +8,7 @@ $(document).ready(function() {
         $contentBoxes = $('.works-list').find('li'),
         $contentImg = $('.works-img'),
         $contentBtn = $('.works-info-btn').find('a'),
+        $toTopBtn = $('.toTop'),
         flag = false;
 
     /*================================================================
@@ -15,10 +16,12 @@ $(document).ready(function() {
       ================================================================*/
     setListeners();
     function setListeners() {
+        $(window).scroll(scrollCallback);
         $infoBtn.on('click', infoBtnCallback);
         $status.hover(statusInCallback, statusOutCallback );
         $contentImg.on('click', preventCallback);
         $contentBtn.on('click', preventCallback);
+        $toTopBtn.on('click', toTopCallback);
 
         function infoBtnCallback(e) {
             e.preventDefault();
@@ -46,7 +49,35 @@ $(document).ready(function() {
                 return false;
             };      
         };
+
+        function toTopCallback(e) {
+            e.preventDefault();
+            $('html, body').animate({
+                scrollTop: 0
+            }, 'slow');
+        };
+
+        scrollCallback();
+        function scrollCallback() {
+            var $wndTop = $(window).scrollTop(),
+                TOP_INDENT = 400;
+            if ($wndTop > TOP_INDENT) {
+                $toTopBtn.addClass('show');
+            } else {
+                $toTopBtn.removeClass('show');
+            }
+        };
     };
+    /*================================================================
+                        >>> For IE 10 <<<  
+    ================================================================*/
+    checkIE10();
+    function checkIE10() {
+        var b = document.documentElement;
+        b.setAttribute('data-useragent',  navigator.userAgent);
+        b.setAttribute('data-platform', navigator.platform );   
+    };
+
 
   
 
